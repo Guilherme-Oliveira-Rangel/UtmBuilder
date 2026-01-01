7427b6a2e6e719239319a8b4c1223c80b7e945af
Guilherme Oliveira <101332889+Guilherme-Oliveira-Rangel@users.noreply.github.com>
2025-12-31 21:28:57 -0300

    Add `Utm` entity class with `Url` and `Campain` properties to core project

---
+++ UtmBuilder.Core/Entities/Utm.cs
@@ -0,0 +1,9 @@
+﻿using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core.Entities;
+
+public class Utm
+{
+    public Url Url { get; private set; }
+    public Campain Campain { get; private set; }
+}
