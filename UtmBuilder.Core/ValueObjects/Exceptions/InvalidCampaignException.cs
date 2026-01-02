4a3eaaf9cd173be2e144dfe2926d9e4404458fd0
Guilherme Oliveira <101332889+Guilherme-Oliveira-Rangel@users.noreply.github.com>
2026-01-01 23:55:08 -0300

    feat: add `InvalidCampaignException` for campaign validation logic

---
+++ UtmBuilder.Core/ValueObjects/Exceptions/InvalidCampaignException.cs
@@ -0,0 +1,15 @@
+﻿namespace UtmBuilder.Core.ValueObjects.Exceptions;
+
+public class InvalidCampaignException : Exception
+{
+    private const string DefaultErrorMessage = "Invalid Campaign";
+
+    public InvalidCampaignException(string message = DefaultErrorMessage) : base(message)
+    {
+    }
+
+    public static void ThrowIfInvalidCampaign(string? item, string message = DefaultErrorMessage)
+    {
+        if (string.IsNullOrWhiteSpace(item)) throw new InvalidUrlException(message);
+    }
+}
