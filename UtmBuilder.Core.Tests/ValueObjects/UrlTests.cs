d4a498efa1923a132484b32b631b1624211c57a6
Guilherme Oliveira <101332889+Guilherme-Oliveira-Rangel@users.noreply.github.com>
2026-01-03 18:29:34 -0300

    refactor(tests): simplify `ValidUrl` constant in `UrlTests`

--- UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -5,9 +5,7 @@ namespace UtmBuilder.Core.Tests.ValueObjects;
 
 public class UrlTests
 {
-    private const string ValidUrl =
-        "https://example.com/products/shoes?utm_source=google&utm_medium=cpc&utm_campaign=spring_sale&utm_id=12345&utm_term=running+shoes&utm_content=ad_variant_a";
-
+    private const string ValidUrl = "https://example.com";
     private const string InvalidUrl = "";
 
     [Fact]
